31e6cc891d16a5a7b300ff85f9e914855bd3f5b4
Areebah-snow <Areebah-snow>
2023-01-16 12:28:31 +0100

    Fixed header

---
+++ code/main.js
@@ -0,0 +1,30 @@
+let typed = new Typed(".typing", {
+    strings: ["","", "Hello(-_-)"],
+    typeSpeed: 300,
+    backSpeed: 90,
+    loop: true,
+    autoplay: {
+        delay: 10000
+    }
+});
+
+let typed2 = new Typed(".typing2", {
+    strings: ["","", "Mhistersax"],
+    typeSpeed: 300,
+    backSpeed: 110,
+    loop: true,
+    autoplay: {
+        delay: 10000
+    }
+});
+
+let typed3 = new Typed(".typing3", {
+    strings: ["","", "I am a Programmer"],
+    typeSpeed: 400,
+    backSpeed: 130,
+    loop: true,
+    autoplay: {
+        delay: 10000
+    }
+});
+
